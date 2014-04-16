2c9e66b02ed43513346f2a8dcef1c8411cb849ee
Eun <eun@su.am>
2014-04-16 15:29:05 +0200

    + Added GLFW Licence

--- DisableMonitor.c
+++ DisableMonitor.c
@@ -33,6 +33,33 @@ extern "C" {
 }
 #endif
 
+// GLFW 3.0 START
+//========================================================================
+// GLFW 3.0 OS X - www.glfw.org
+//------------------------------------------------------------------------
+// Copyright (c) 2002-2006 Marcus Geelnard
+// Copyright (c) 2006-2010 Camilla Berglund <elmindreda@elmindreda.org>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would
+//    be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+//    be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+//========================================================================
 io_service_t IOServicePortFromCGDisplayID(CGDirectDisplayID displayID)
 {
     io_iterator_t iter;
@@ -131,7 +158,7 @@ char* getDisplayName(CGDirectDisplayID displayID)
     
     return name;
 }
-
+// GLFW 3.0 END
 
 int main(int argc, const char * argv[])
 {
