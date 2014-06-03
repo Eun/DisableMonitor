6dbcd33c7903f2907fc2f833c1bc4cdac6c0b5d6
Eun <eun@su.am>
2014-06-03 10:44:41 +0200

    * Resolution can now be changed! * Bugfixes * improved stability

--- DisableMonitorAppDelegate.h
+++ DisableMonitorAppDelegate.h
@@ -1,10 +1,20 @@
-//
-//  DisableMonitorAppDelegate.h
-//  DisableMonitor
-//
-//  Created by Aravindkumar Rajendiran on 10-04-17.
-//  Copyright 2010 Grapewave. All rights reserved.
-//
+/**
+ * DisableMonitor, Disable Monitors on Mac
+ *
+ * Copyright (C) 2014 Tobias Salzmann
+ *
+ * DisableMonitor is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * DisableMonitor is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with DisableMonitor. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Tobias Salzmann
+ */
 
 #import <Cocoa/Cocoa.h>
 
--- DisableMonitorAppDelegate.m
+++ DisableMonitorAppDelegate.m
@@ -1,10 +1,20 @@
-//
-//  DisableMonitorAppDelegate.m
-//  DisableMonitor
-//
-//  Created by Aravindkumar Rajendiran on 10-04-17.
-//  Copyright 2010 Grapewave. All rights reserved.
-//
+/**
+ * DisableMonitor, Disable Monitors on Mac
+ *
+ * Copyright (C) 2014 Tobias Salzmann
+ *
+ * DisableMonitor is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * DisableMonitor is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details. You should have received a copy of the GNU
+ * General Public License along with DisableMonitor. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Authors: Tobias Salzmann
+ */
 
 #import "DisableMonitorAppDelegate.h"
 #import <IOKit/graphics/IOGraphicsLib.h>
@@ -13,18 +23,24 @@
 
 @synthesize window;
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
-}
-
 
 extern io_service_t IOServicePortFromCGDisplayID(CGDirectDisplayID displayID);
 extern CGError CGSConfigureDisplayEnabled(CGDisplayConfigRef, CGDirectDisplayID, bool);
 
+
+NSMutableArray *monitorConfigs;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    monitorConfigs = NULL;
+}
+
+
+
+static void KeyArrayCallback(const void* key, const void* value, void* context) { CFArrayAppendValue(context, key);  }
+
 NSString* screenNameForDisplay(CGDirectDisplayID displayID)
 {
     NSString *screenName = nil;
-    
     io_service_t service = IOServicePortFromCGDisplayID(displayID);
     if (service)
     {
@@ -40,6 +56,26 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
     return [screenName autorelease];
 }
 
+-(void)ShowError:(NSString*)error
+{
+    
+    if (![NSThread isMainThread])
+    {
+        [self performSelectorOnMainThread:@selector(ShowError:) withObject:error waitUntilDone:NO];
+        return;
+    }
+    
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setInformativeText:error];
+    [alert addButtonWithTitle:@"OK"];
+    [alert setAlertStyle:NSCriticalAlertStyle];
+    [alert setMessageText:@"Error"];
+    [alert runModal];
+    [alert release];
+}
+
+#define ShowError(...) [self ShowError:[NSString stringWithFormat:__VA_ARGS__]];
+
 -(void)ToggleMonitor:(CGDirectDisplayID) display enabled:(Boolean) enabled
 {
     CGError err;
@@ -48,19 +84,106 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
         err = CGBeginDisplayConfiguration (&config);
         if (err != 0)
         {
-            NSLog(@"Error in CGBeginDisplayConfiguration: %d\n", err);
+            ShowError(@"Error in CGBeginDisplayConfiguration: %d",err);
             return;
         }
         err = CGSConfigureDisplayEnabled(config, display, enabled);
         if (err != 0)
         {
-            NSLog(@"Error in CGSConfigureDisplayEnabled: %d\n", err);
+            ShowError(@"Error in CGSConfigureDisplayEnabled: %d", err);
             return;
         }
         err = CGCompleteDisplayConfiguration(config, kCGConfigurePermanently);
         if (err != 0)
         {
-            NSLog(@"Error in CGCompleteDisplayConfiguration: %d\n", err);
+            ShowError(@"Error in CGCompleteDisplayConfiguration: %d", err);
+            return;
+        }
+    }
+    @catch (NSException *exception) {
+        
+    }
+}
+
+
+-(void)SetMonitorRes:(CGDirectDisplayID) display title:(NSString*) title
+{
+    CGError err;
+    CGDisplayConfigRef config;
+    
+    
+    size_t w;
+    size_t h;
+    size_t d;
+    int r;
+    int numConverted = sscanf([title UTF8String], "%lux%lux%lu@%d", &w, &h, &d, &r);
+    if (numConverted != 4) {
+        numConverted = sscanf([title UTF8String], "%lux%lux%lu", &w, &h, &d);
+        if (numConverted != 3) {
+            ShowError(@"Error: the mode '%s' couldn't be parsed", [title UTF8String]);
+            return;
+        } else {
+            r=60.0;
+        }
+    }
+    
+    @try {
+        
+        
+        CFArrayRef allModes = CGDisplayCopyAllDisplayModes(display, NULL);
+        if (allModes == NULL) {
+            ShowError(@"Error: failed trying to look up modes for display %u", display);
+            return;
+        }
+        
+        CGDisplayModeRef newMode = NULL;
+        CGDisplayModeRef possibleMode;
+        size_t pw;
+        size_t ph;
+        size_t pd;
+        int pr;
+        int looking = 1;
+        int i;
+        for (i = 0 ; i < CFArrayGetCount(allModes) && looking; i++) {
+            possibleMode = (CGDisplayModeRef)CFArrayGetValueAtIndex(allModes, i);
+            pw = CGDisplayModeGetWidth(possibleMode);
+            ph = CGDisplayModeGetHeight(possibleMode);
+            pd = bitDepth(possibleMode);
+            pr = (int)CGDisplayModeGetRefreshRate(possibleMode);
+            if (pw == w &&
+                ph == h &&
+                pd == d &&
+                pr == r) {
+                looking = 0;
+                newMode = possibleMode;
+            }
+        }
+        CFRelease(allModes);
+        
+        if (newMode == NULL) {
+            ShowError(@"Error: mode %lux%lux%lu@%d not available on display %u",
+                  w, h, d, r, display);
+            return;
+        }
+        
+        err = CGBeginDisplayConfiguration (&config);
+        if (err != 0)
+        {
+            ShowError(@"Error in CGBeginDisplayConfiguration: %d\n", err);
+            return;
+        }
+    
+        
+        err = CGConfigureDisplayWithDisplayMode(config, display, newMode, NULL);
+        if (err != 0)
+        {
+            ShowError(@"Error in CGConfigureDisplayWithDisplayMode: %d\n", err);
+            return;
+        }
+        err = CGCompleteDisplayConfiguration(config, kCGConfigurePermanently);
+        if (err != 0)
+        {
+            ShowError(@"Error in CGCompleteDisplayConfiguration: %d\n", err);
             return;
         }
     }
@@ -72,9 +195,8 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
 -(void)MonitorClicked:(id) sender
 {
     NSMenuItem * item = (NSMenuItem*)sender;
-    
     CGDirectDisplayID display = (CGDirectDisplayID)[item tag];
-    Boolean active = CGDisplayIsActive(display);
+    BOOL active = CGDisplayIsActive(display);
     
     if (active == true)
     {
@@ -84,7 +206,7 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
         
         if (err == 0 && nDisplays - 1 == 0)
         {
-            NSAlert *alert = [[[NSAlert alloc] init] autorelease];
+            NSAlert *alert = [[NSAlert alloc] init];
             [alert setInformativeText:@"You are disabling your last active monitor, you wont be able to see anything continue?"];
             [alert addButtonWithTitle:@"OK"];
             [alert addButtonWithTitle:@"Cancel"];
@@ -102,9 +224,30 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         [self ToggleMonitor:display enabled:!active];
     });
-    
 }
 
+-(void)MonitorResolution:(id) sender
+{
+    NSMenuItem * item = (NSMenuItem*)sender;
+    BOOL active = CGDisplayIsActive([item tag]);
+    
+    if (active == true)
+    {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            [self SetMonitorRes:[item tag] title:[item title]];
+        });
+    }
+    else
+    {
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert setInformativeText:@"Monitor is not active!\nThe monitor needs to be active to change the resolution!"];
+        [alert addButtonWithTitle:@"OK"];
+        [alert setAlertStyle:NSCriticalAlertStyle];
+        [alert setMessageText:@"Warning"];
+        [alert runModal];
+        [alert release];
+    }
+}
 
 -(void)DetectMonitors:(id) sender
 {
@@ -123,8 +266,34 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
 }
 
 
+size_t bitDepth(CGDisplayModeRef mode) {
+    size_t depth = 0;
+	CFStringRef pixelEncoding = CGDisplayModeCopyPixelEncoding(mode);
+    // my numerical representation for kIO16BitFloatPixels and kIO32bitFloatPixels
+    // are made up and possibly non-sensical
+    if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(kIO32BitFloatPixels), kCFCompareCaseInsensitive)) {
+        depth = 96;
+    } else if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(kIO64BitDirectPixels), kCFCompareCaseInsensitive)) {
+        depth = 64;
+    } else if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(kIO16BitFloatPixels), kCFCompareCaseInsensitive)) {
+        depth = 48;
+    } else if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(IO32BitDirectPixels), kCFCompareCaseInsensitive)) {
+        depth = 32;
+    } else if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(kIO30BitDirectPixels), kCFCompareCaseInsensitive)) {
+        depth = 30;
+    } else if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(IO16BitDirectPixels), kCFCompareCaseInsensitive)) {
+        depth = 16;
+    } else if (kCFCompareEqualTo == CFStringCompare(pixelEncoding, CFSTR(IO8BitIndexedPixels), kCFCompareCaseInsensitive)) {
+        depth = 8;
+    }
+    CFRelease(pixelEncoding);
+    return depth;
+}
+
 - (void)menuNeedsUpdate:(NSMenu *)menu
 {
+    if (menu != statusMenu)
+        return;
     
     [statusMenu removeAllItems];
     CGDirectDisplayID    displays[0x10];
@@ -135,16 +304,17 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
     if (err == 0 && nDisplays > 0)
     {
         NSMutableArray *monitors = [[NSMutableArray alloc] init];
-        NSMutableArray *monitorIDs = [[NSMutableArray alloc] init];
         for (int i = 0; i < nDisplays; i++)
         {
             NSString *name = screenNameForDisplay(displays[i]);
             if (name != nil)
             {
                 [monitors addObject: name];
-                [monitorIDs addObject:[NSNumber numberWithInt:(CGDirectDisplayID)displays[i]]];
-                
-                
+            }
+            
+            else
+            {
+                [monitors addObject: [NSString stringWithFormat:@"Display #%d", i + 1]];
             }
         }
         
@@ -153,6 +323,8 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
             int num = 0;
             int index = 1;
             
+            if (!CGDisplayIsOnline(displays[i]))
+                continue;
             
             for (int j = 0; j < monitors.count; j++)
             {
@@ -172,18 +344,103 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
             else
                 name = monitors[i];
             
-            NSMenuItem *displayItem = [[NSMenuItem alloc] initWithTitle: name  action:@selector(MonitorClicked:) keyEquivalent:@""];
-            if (CGDisplayIsActive(displays[i]))
+            //NSMenuItem *displayItem = [[NSMenuItem alloc] initWithTitle: name  action:@selector(MonitorClicked:) keyEquivalent:@""];
+            NSMenuItem *displayItem = [[NSMenuItem alloc] initWithTitle: name  action:nil keyEquivalent:@""];
+            BOOL bActive = CGDisplayIsActive(displays[i]);
+            if (bActive)
                 [displayItem setState:NSOnState];
             else
                 [displayItem setState:NSOffState];
-            [displayItem setTag:displays[i]];
+            
+            NSMenu *subMenu = [[NSMenu alloc] init];
+            NSMenuItem *subItem;
+
+            
+
+
+            
+            CFArrayRef allModes = CGDisplayCopyAllDisplayModes(displays[i], NULL);
+            
+            if (allModes != NULL)
+            {
+                CGDisplayModeRef currentMode = CGDisplayCopyDisplayMode(displays[i]);
+                
+                
+                size_t current_w = CGDisplayModeGetWidth(currentMode);
+                size_t current_h = CGDisplayModeGetHeight(currentMode);
+                size_t current_d = bitDepth(currentMode);
+                int current_r = (int)CGDisplayModeGetRefreshRate(currentMode);
+                
+                
+                for (int j = 0, len = CFArrayGetCount(allModes); j < len; j++) {
+                    CGDisplayModeRef mode = (CGDisplayModeRef)CFArrayGetValueAtIndex(allModes, j);
+                    
+                    
+
+                    size_t w = CGDisplayModeGetWidth(mode);
+                    size_t h = CGDisplayModeGetHeight(mode);
+                    size_t d = bitDepth(mode);
+                    int r = (int)CGDisplayModeGetRefreshRate(mode);
+                    
+                    
+                    subItem = [[NSMenuItem alloc] initWithTitle: [NSString stringWithFormat:@"%lux%lux%lu@%d", w, h, d, r] action:@selector(MonitorResolution:)  keyEquivalent:@""];
+                    [subItem setTag: displays[i]];
+                    NSUInteger newIndex = [[subMenu itemArray] indexOfObject:subItem
+                                                 inSortedRange:(NSRange){0, [[subMenu itemArray] count]}
+                                                       options:NSBinarySearchingInsertionIndex
+                                                     usingComparator:^(id a, id b) {
+                                                         return [[b title] compare: [a title] options:NSNumericSearch];
+                                                     }];
+                    
+                    
+                    if (current_w == w && current_h == h && current_d == d && current_r == r)
+                        [subItem setState:NSOnState];
+                    else
+                        [subItem setState:NSOffState];
+                    [subMenu insertItem:subItem atIndex:newIndex];
+                    [subItem release];
+                    
+                }
+
+                
+                CFRelease(allModes);
+            }
+            
+
+            if (bActive)
+            {
+                subItem = [[NSMenuItem alloc] initWithTitle:@"Disable" action:@selector(MonitorClicked:)  keyEquivalent:@""];
+                [subItem setTag:displays[i]];
+                [subMenu insertItem:subItem atIndex:0];
+                [subItem release];
+            }
+            else
+            {
+                subItem = [[NSMenuItem alloc] initWithTitle:@"Enable" action:@selector(MonitorClicked:)  keyEquivalent:@""];
+                [subItem setTag:displays[i]];
+                [subMenu insertItem:subItem atIndex:0];
+                [subItem release];
+            }
+            
+            [subMenu insertItem:[NSMenuItem separatorItem] atIndex:1];
+
+            /*
+            [subMenu insertItem:[NSMenuItem separatorItem] atIndex:[[subMenu itemArray] count]];
+            
+            subItem = [[NSMenuItem alloc] initWithTitle:@"Custom" action:@selector(MonitorCustomResolution:)  keyEquivalent:@""];
+            [subItem setTag:displays[i]];
+            [subMenu insertItem:subItem atIndex:[[subMenu itemArray] count]];
+            [subItem release];
+             */
+            
+            [subMenu setDelegate:self];
+            
+            [displayItem setSubmenu:subMenu];
             [statusMenu addItem:displayItem];
             [displayItem release];
 
         }
         [monitors release];
-        [monitorIDs release];
         
     }
     else
@@ -199,6 +456,7 @@ NSString* screenNameForDisplay(CGDirectDisplayID displayID)
     [statusMenu addItem:menuItem];
     [menuItem release];
 }
+
 
 -(void)awakeFromNib{
     
