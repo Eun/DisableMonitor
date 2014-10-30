e1096d90f03f0eaf52d1a39f71b50ab4a7e05695
Eun <eun@su.am>
2014-10-30 10:32:52 +0100

    Ready for Yosemite Dark Mode

--- DisableMonitorAppDelegate.m
+++ DisableMonitorAppDelegate.m
@@ -27,7 +27,84 @@
 #import "DisplayIDAndName.h"
 #include <stdlib.h>
 
+@implementation NSImage (NegativeImage)
 
+- (NSImage *)negativeImage
+{
+    // get width and height as integers, since we'll be using them as
+    // array subscripts, etc, and this'll save a whole lot of casting
+    CGSize size = self.size;
+    int width = size.width;
+    int height = size.height;
+    
+    // Create a suitable RGB+alpha bitmap context in BGRA colour space
+    CGColorSpaceRef colourSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *memoryPool = (unsigned char *)calloc(width*height*4, 1);
+    CGContextRef context = CGBitmapContextCreate(memoryPool, width, height, 8, width * 4, colourSpace, kCGBitmapByteOrder32Big | kCGImageAlphaPremultipliedLast);
+    CGColorSpaceRelease(colourSpace);
+    
+    // draw the current image to the newly created context
+    
+    CGImageSourceRef source;
+    
+    source = CGImageSourceCreateWithData((CFDataRef)[self TIFFRepresentation], NULL);
+    CGImageRef maskRef =  CGImageSourceCreateImageAtIndex(source, 0, NULL);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), maskRef);
+    
+    // run through every pixel, a scan line at a time...
+    for(int y = 0; y < height; y++)
+    {
+        // get a pointer to the start of this scan line
+        unsigned char *linePointer = &memoryPool[y * width * 4];
+        
+        // step through the pixels one by one...
+        for(int x = 0; x < width; x++)
+        {
+            // get RGB values. We're dealing with premultiplied alpha
+            // here, so we need to divide by the alpha channel (if it
+            // isn't zero, of course) to get uninflected RGB. We
+            // multiply by 255 to keep precision while still using
+            // integers
+            int r, g, b;
+            if(linePointer[3])
+            {
+                r = linePointer[0] * 255 / linePointer[3];
+                g = linePointer[1] * 255 / linePointer[3];
+                b = linePointer[2] * 255 / linePointer[3];
+            }
+            else
+                r = g = b = 0;
+            
+            // perform the colour inversion
+            r = 255 - r;
+            g = 255 - g;
+            b = 255 - b;
+            
+            // multiply by alpha again, divide by 255 to undo the
+            // scaling before, store the new values and advance
+            // the pointer we're reading pixel data from
+            linePointer[0] = r * linePointer[3] / 255;
+            linePointer[1] = g * linePointer[3] / 255;
+            linePointer[2] = b * linePointer[3] / 255;
+            linePointer += 4;
+        }
+    }
+    
+    // get a CG image from the context, wrap that into a
+    // UIImage
+    CGImageRef cgImage = CGBitmapContextCreateImage(context);
+    NSImage *returnImage = [[NSImage alloc] initWithCGImage:cgImage size: NSZeroSize];
+    // clean up
+    CGImageRelease(cgImage);
+    CGContextRelease(context);
+    free(memoryPool);
+    
+    // and return
+    return returnImage;
+}
+
+@end
 
 @implementation DisableMonitorAppDelegate
 
@@ -1106,14 +1183,47 @@ extern void IOFBCreateOverrides(void* connectRef);*/
 }
 
 
+- (BOOL)isInDarkMode
+{
+    NSDictionary *dict = [[NSUserDefaults standardUserDefaults] persistentDomainForName:NSGlobalDomain];
+    id style = [dict objectForKey:@"AppleInterfaceStyle"];
+    return ( style && [style isKindOfClass:[NSString class]] && NSOrderedSame == [style caseInsensitiveCompare:@"dark"] );
+}
+
+-(void)darkModeChanged:(NSNotification *)notif
+{
+    NSImage *statusImage = [self imageResize:[[NSImage imageNamed:@"icon.icns"] copy] newSize:NSMakeSize(20, 20)];
+    
+    if ([self isInDarkMode])
+    {
+        NSImage *normalImage = statusImage;
+        statusImage = [normalImage negativeImage];
+        [normalImage release];
+    }
+    
+    [statusItem setImage:statusImage];
+}
+
+
+
 -(void)awakeFromNib{
     customResolution = nil;
     statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength] retain];
     [statusMenu setDelegate:self];
     [statusItem setMenu:statusMenu];
-    [statusItem setImage:[self imageResize:[[NSImage imageNamed:@"icon.icns"] copy] newSize:NSMakeSize(20, 20)]];
+    NSImage *statusImage = [self imageResize:[[NSImage imageNamed:@"icon.icns"] copy] newSize:NSMakeSize(20, 20)];
+    
+    if ([self isInDarkMode])
+    {
+        NSImage *normalImage = statusImage;
+        statusImage = [normalImage negativeImage];
+        [normalImage release];
+    }
+    
+    [statusItem setImage:statusImage];
     [statusItem setHighlightMode:YES];
-   
+    [[NSDistributedNotificationCenter defaultCenter] addObserver:self selector:@selector(darkModeChanged:) name:@"AppleInterfaceThemeChangedNotification" object:nil];
+
     
     
 }
